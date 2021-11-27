af400e9d17f9f5e08f9a13035b49333eb1b96f89
breno777-cyber <brenoryan21@hotmail.com>
2021-11-27 17:01:13 -0300

    pagina de cliente

---
+++ view/meubancoangular/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { DepositoComponent } from './pages/deposito/deposito.component';
+import { SaqueComponent } from './pages/saque/saque.component';
+import { ExtratoComponent } from './pages/extrato/extrato.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component:HomeComponent, pathMatch: 'full'},
+  { path: 'extrato', component:ExtratoComponent},
+  { path: 'saque', component:SaqueComponent},
+  { path: 'deposito', component:DepositoComponent},
+  { path: 'clientes', component:ClientesComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
